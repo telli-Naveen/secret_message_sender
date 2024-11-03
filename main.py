0197bcc5ae955c1f9356794b86b1954d93c8d335
telli-Naveen <112822004+telli-Naveen@users.noreply.github.com>
2024-11-03 21:46:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/encrypt", methods=["POST", "GET"])
+@app.route("https://secret-message-sender.onrender.com/encrypt", methods=["POST", "GET"])
 def encrypt():
     if request.method == "POST":
         input_string = request.form['Message']
@@ -26,14 +26,14 @@ def encrypt():
 
             encrypted_string += chr(new_ascii)
 
-        encrypted_string = "http://127.0.0.1:5000/encrypt/" + encrypted_string
+        encrypted_string = "https://secret-message-sender.onrender.com/encrypt/" + encrypted_string
         return render_template('index.html', result=encrypted_string)
 
 
     return render_template("index.html")
 
 
-@app.route("/encrypt/<data>", methods=["POST", "GET"])
+@app.route("https://secret-message-sender.onrender.com/encrypt/<data>", methods=["POST", "GET"])
 def decrypt(data):
     if request.method == "POST":
         encrypted_string = request.form["Message"]
